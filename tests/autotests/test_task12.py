24fcf1f4bf457a807c0e52a7e5420655028d14f3
Dabzelos <dabzelos@gmail.com>
2025-10-27 00:26:35 +0300

    sixth task

--- tests/autotests/test_task12.py
+++ tests/autotests/test_task12.py
@@ -17,8 +17,8 @@ from helper import generate_rnd_start_and_final, generate_rnd_dense_graph
 from constants import LABELS
 
 try:
-    from project.task7 import matrix_based_cfpq
-    from project.task12 import typing_program, exec_program
+    from project.task_7 import matrix_based_cfpq
+    from project.task_12 import typing_program, exec_program
 except ImportError:
     pytestmark = pytest.mark.skip("Task 12 is not ready to test!")
 
